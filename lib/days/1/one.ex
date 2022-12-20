db794db7859f6b1ff6c14a457d8eb9924d8cf0fd
JP <jp.bonner@bonner.jp>
2022-12-20 11:48:24 -0500

    day 1: calorie counting, files util, strings util

--- lib/days/1/one.ex
+++ lib/days/1/one.ex
@@ -5,32 +5,44 @@ defmodule Aoc22.One do
   """
   alias Aoc22.Util.Files
   alias Aoc22.Util.Strings
+
   @day 1
 
+  @type calorie_inventory :: [integer()]
+
+  @type calorie_inventory_list :: [calorie_inventory()]
+
   def run() do
-    one()
-    two()
+    {one(), two()}
   end
 
   def one(input_type \\ :input, input_num \\ 1) do
     parse_input(input_type, input_num)
+    |> build_calorie_inventory_list()
     |> Enum.map(&Enum.sum/1)
     |> Enum.max()
   end
 
   def two(input_type \\ :input, input_num \\ 1) do
     parse_input(input_type, input_num)
+    |> build_calorie_inventory_list()
+    |> Enum.map(&Enum.sum/1)
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.sum()
+  end
+
+  @spec build_calorie_inventory_list(String.t()) :: calorie_inventory_list()
+  defp build_calorie_inventory_list(calorie_counts) do
+    Strings.tokenize(calorie_counts, &build_calorie_inventory/1, "\n\n")
+  end
+
+  @spec build_calorie_inventory(String.t()) :: calorie_inventory()
+  defp build_calorie_inventory(counts) do
+    Strings.tokenize(counts, &String.to_integer/1, "\n")
   end
 
   defp parse_input(input_type, input_num) do
-    tokenizer = fn chunk ->
-      Strings.tokenize(chunk, &String.to_integer/1, "\n")
-    end
-
     Files.file_for(@day, input_type, input_num)
-    |> Strings.tokenize(
-      tokenizer,
-      "\n\n"
-    )
   end
 end
--- lib/util/files.ex
+++ lib/util/files.ex
@@ -1,7 +1,13 @@
 defmodule Aoc22.Util.Files do
-  @spec file_for(integer, atom, integer) :: <<_::64, _::_*8>>
+  @spec file_for(integer, atom, integer) :: String.t()
   def file_for(day_num, input_type, input_num) do
-    "#{File.cwd!()}/lib/days/#{Integer.to_string(day_num)}/#{Atom.to_string(input_type)}/#{Integer.to_string(input_num)}.txt"
+    filename =
+      case input_type do
+        :example -> "example_#{input_num}.txt"
+        :input -> "input.txt"
+      end
+
+    "#{File.cwd!()}/lib/days/#{Integer.to_string(day_num)}/data/#{filename}"
     |> File.read!()
   end
 end
